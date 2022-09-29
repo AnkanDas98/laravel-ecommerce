7f3edd99b0953f1e9ce20c09ac19596a3f7be903
Ankan Das <ankan.das96@gmail.com>
2022-09-29 20:22:52 +0600

    added role base authorization and authentication

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Database\Seeders\LaratrustSeeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // \App\Models\User::factory(10)->create();
+
+    //    $user = \App\Models\User::create([
+    //         'name' => 'ankan',
+    //         'email' => 'ankan@gmail.com',
+    //         'password' => Hash::make('ank123456')
+    //     ]);
+
+    //     $user->attachRole('admin');
+
+        // $this->call(LaratrustSeeder::class);
+    }
+}
